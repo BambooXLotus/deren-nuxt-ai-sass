5e37e4245b53b08e792ddb9303bf2970b010af63
Deren Somsanith <derensomsanith@gmail.com>
2024-04-19 10:44:03 -0500

    added heading component

---
+++ components/Heading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="px-4 lg:px-8 flex items-center gap-x-3 pb-8">
+    <div :class="`p-2 w-fit rounded-md ${bgColor}`">
+      <Icon :name="icon" :class="`w-10 h-10 ${iconColor}`" />
+    </div>
+    <div>
+      <h2 class="text-3xl font-bold">{{ title }}</h2>
+      <p class="text-sm text-muted-foreground">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+type HeadingProps = {
+  title: string
+  description: string
+  icon: string
+  iconColor?: string
+  bgColor?: string
+}
+
+defineProps<HeadingProps>()
+</script>
+
+<style scoped></style>
